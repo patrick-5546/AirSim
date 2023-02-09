1c2c678051367f0d6ebbe6ca7a77e65aaa98140c
Patrick Creighton <pcreighton429@gmail.com>
2023-02-08 18:07:25 -0800

    Add logs to car environment

--- PythonClient/reinforcement_learning/airgym/envs/car_env.py
+++ PythonClient/reinforcement_learning/airgym/envs/car_env.py
@@ -2,11 +2,18 @@ import setup_path
 import airsim
 import numpy as np
 import math
+import os
+import shutil
 import time
 
 import gym
 from gym import spaces
 from airgym.envs.airsim_env import AirSimEnv
+
+
+LOG_DIR = '/home/pcreighton/git/nserc/AirSim/PythonClient/reinforcement_learning/car_env_logs'
+ACTIONS_LOG_NAME = 'actions.txt'
+ACTIONS_LOG_PATH = os.path.join(LOG_DIR, ACTIONS_LOG_NAME)
 
 
 class AirSimCarEnv(AirSimEnv):
@@ -34,6 +41,20 @@ class AirSimCarEnv(AirSimEnv):
         self.car_controls = airsim.CarControls()
         self.car_state = None
 
+        os.makedirs(LOG_DIR, exist_ok=True)
+        self.episode = 0
+        self.step_obs = False
+        self.obs = []
+        self.action = ''
+        self.actions = []
+        for filename in os.listdir(LOG_DIR):
+            filepath = os.path.join(LOG_DIR, filename)
+            if os.path.isdir(filepath):
+                print('delete episode directory', filepath)
+                shutil.rmtree(filepath)
+        with open(ACTIONS_LOG_PATH, 'w') as f:
+            f.write('actions:\n')
+
     def _setup_car(self):
         self.car.reset()
         self.car.enableApiControl(True)
@@ -50,16 +71,22 @@ class AirSimCarEnv(AirSimEnv):
         if action == 0:
             self.car_controls.throttle = 0
             self.car_controls.brake = 1
+            self.action = 'stop'
         elif action == 1:
             self.car_controls.steering = 0
+            self.action = 'north'
         elif action == 2:
             self.car_controls.steering = 0.5
+            self.action = 'east'
         elif action == 3:
             self.car_controls.steering = -0.5
+            self.action = 'west'
         elif action == 4:
             self.car_controls.steering = 0.25
+            self.action = 'northeast'
         else:
             self.car_controls.steering = -0.25
+            self.action = 'northwest'
 
         self.car.setCarControls(self.car_controls)
         time.sleep(1)
@@ -69,9 +96,14 @@ class AirSimCarEnv(AirSimEnv):
         img1d = 255 / np.maximum(np.ones(img1d.size), img1d)
         img2d = np.reshape(img1d, (response.height, response.width))
 
-        from PIL import Image
+        from PIL import Image, ImageEnhance
 
         image = Image.fromarray(img2d)
+        if self.step_obs:
+            img = image.convert("L")
+            # filter = ImageEnhance.Brightness(img)
+            # img = filter.enhance(1.2)
+            self.obs.append(img)
         im_final = np.array(image.resize((84, 84)).convert("L"))
 
         return im_final.reshape([84, 84, 1])
@@ -135,14 +167,36 @@ class AirSimCarEnv(AirSimEnv):
 
         return reward, done
 
+    def _log(self, done, log_obs=False, log_actions=True):
+        self.actions.append(self.action)
+        if done:
+            if log_obs:
+                ep_log_dir = os.path.join(LOG_DIR, str(self.episode))
+                os.makedirs(ep_log_dir, exist_ok=True)
+                # print(self.episode, len(self.obs), sep=': ')
+                for i, img in enumerate(self.obs):
+                    img_name = os.path.join(ep_log_dir, f'{i}.jpg')
+                    img.save(img_name)
+
+            if log_actions:
+                with open(ACTIONS_LOG_PATH, 'a') as f:
+                    actions = ' '.join(f'{a : >10}' for a in self.actions)
+                    f.write(f'{self.episode}: {actions}\n')
+
+            self.episode += 1
+
     def step(self, action):
         self._do_action(action)
+        self.step_obs = True
         obs = self._get_obs()
+        self.step_obs = False
         reward, done = self._compute_reward()
-
+        self._log(done)
         return obs, reward, done, self.state
 
     def reset(self):
         self._setup_car()
         self._do_action(1)
+        self.obs = []
+        self.actions = []
         return self._get_obs()
